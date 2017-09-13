3be139b4425c1e526c2ecb29a347213321cbac66
Andreja Korade <andreja@korade.si>
2017-09-13 12:31:27 +0200

    first commit

---
+++ small-letters.py
@@ -0,0 +1,9 @@
+
+text = "Those LETTers aRe aLL miXED Up. ThIS mUsT geT In THe order SOMEhow...."
+print text
+print" "
+print"     with lower letters: "
+print text.lower()
+print" "
+print"     with upper letters: "
+print text.upper()
